4baee0c6d1f6b932c2845ad84172cc65f91e81ce
Russell Dwarshuis <rjd@merit.edu>
2013-08-05 11:16:25 -0400

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.CiscoPluggableOptics"
-VERSION = "1.0.0"
+VERSION = "1.0.2"
 AUTHOR = "Russell Dwarshuis"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
