fcea40d26fec6b2f14049422b1a74b785b9ba628
Russell Dwarshuis <rjd@merit.edu>
2013-10-17 18:12:27 -0400

    models interfaces successfully.  bias current sensors not tested, sample data did not include any

--- ZenPacks/community/CiscoPluggableOptics/modeler/plugins/community/cmd/CiscoPluggableOpticsCmd.py
+++ ZenPacks/community/CiscoPluggableOptics/modeler/plugins/community/cmd/CiscoPluggableOpticsCmd.py
@@ -19,9 +19,13 @@ from Products.DataCollector.plugins.CollectorPlugin import PythonPlugin
 from Products.DataCollector.plugins.DataMaps import ObjectMap, RelationshipMap
 
 
+
+
 #class CiscoPluggableOpticsCmd(CommandPlugin):
 class CiscoPluggableOpticsCmd(PythonPlugin):
-    "Map Cisco Entity sensors on intefaces to the python class for them"
+    """Map Cisco Entity sensors on intefaces to the python class for them.
+Assumes data that looks like file sample_output.txt"""
+
 
     # The command to run.
     command =   "terminal length 0\n" \
@@ -53,6 +57,7 @@ class CiscoPluggableOpticsCmd(PythonPlugin):
         # lines that match these indicate a sensor type change
         temperature_re = re.compile('celsius',re.IGNORECASE)
         voltage_re = re.compile('volt',re.IGNORECASE)
+        bias_re = re.compile('\s+\(mA\)\s+')
         transmit_re = re.compile('transmit',re.IGNORECASE)
         receive_re = re.compile('receive',re.IGNORECASE)
         intf_re = re.compile(r'^(g[ie]*\d+\S+)',re.IGNORECASE)
@@ -64,21 +69,25 @@ class CiscoPluggableOpticsCmd(PythonPlugin):
         for line in results.split('\n'):
             log.debug('Processing line %s' % line)
             if temperature_re.search(line):
-               log.debug('changed current_sensor: %s' % current_sensor)
                current_sensor = 'cmdCelsius'
                sensor[current_sensor] = []
-            if voltage_re.search(line):
                log.debug('changed current_sensor: %s' % current_sensor)
+            if voltage_re.search(line):
                current_sensor = 'cmdVoltsdc'
                sensor[current_sensor] = []
-            if transmit_re.search(line):
                log.debug('changed current_sensor: %s' % current_sensor)
+            if bias_re.search(line):
+               current_sensor = 'cmdMilliamps'
+               sensor[current_sensor] = []
+               log.debug('changed current_sensor: %s' % current_sensor)
+            if transmit_re.search(line):
                current_sensor = 'cmdTxDbm'
                sensor[current_sensor] = []
-            if receive_re.search(line):
                log.debug('changed current_sensor: %s' % current_sensor)
+            if receive_re.search(line):
                current_sensor = 'cmdRxDbm'
                sensor[current_sensor] = []
+               log.debug('changed current_sensor: %s' % current_sensor)
 
             line.strip(' \n')
             if intf_re.search(line):
@@ -117,7 +126,10 @@ class CiscoPluggableOpticsCmd(PythonPlugin):
                 break
         log.debug('interface_pos %d descr_pos %d' % (interface_pos,descr_pos))
 
-        # loop over lines and find interface description
+        # loop over lines and find interface description.
+        # This code assumes description comes last and interfaces names do
+        # not have whitespace in them.  If that's not the case, more position
+        # finding will be necessary
         intf_descr = {}
         if descr_pos == 0:
             log.info("Can't determine interface descriptions")
@@ -147,21 +159,39 @@ class CiscoPluggableOpticsCmd(PythonPlugin):
                           pprint.pformat(intf_descr))
             
 
-        return
-       
         rm = self.relMap()
         for sensor_type in sensor:
-            om = self.objectMap()
-            om.id = self.prepId(intf)
-            om.title = intf
-            om.snmpindex = 0
-            om.ifDescr = somethingortheother
-            om.ifAlias = somethingelse
-            om.ifIndex = 0
-            om.entSensorType = sensorType['intf']
-            om.entSensorScale = sensorScale['intf']
-            om.entSensorPrecision = sensorPrecision['intf']
-            om.monitor = True
-            rm.append(om)
+            sensor_title = ''
+            if sensor_type == 'cmdCelsius':
+                sensor_title = 'Module Temperature Sensor'
+            elif sensor_type == 'cmdVoltsdc':
+                sensor_title = 'Supply Voltage Sensor'
+            elif sensor_type == 'cmdMilliamps':
+                sensor_title = 'Bias Current Sensor'
+            elif sensor_type == 'cmdRxDbm':
+                sensor_title = 'Receive Power Sensor'
+            elif sensor_type == 'cmdTxDbm':
+                sensor_title = 'Transmit Power Sensor'
+            else:
+                log.info('Unknown sensor type %s' % sensor_type)
+                continue
+
+            for intf in sensor[sensor_type]:
+                om = self.objectMap()
+                om.id = self.prepId("%s %s" % (intf,sensor_type))
+                om.title = intf + ' ' + sensor_title
+                om.snmpindex = 0
+                om.ifDescr = intf
+                if intf in intf_descr:
+                    om.ifAlias = intf_descr[intf]
+                else:
+                    log.info('No description found for interface %s' % intf)
+                    om.ifAlias = ''
+                om.physDescr  = "%s %s" % (intf,sensor_type)
+                om.ifIndex = 0
+                om.entSensorType = sensor_type
+                om.entSensorScale = 0
+                om.entSensorPrecision = 0
+                om.monitor = True
+                rm.append(om)
         return rm
-            
