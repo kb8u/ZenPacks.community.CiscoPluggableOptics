c69c9e5f0f0796177a1f00d9ea992948a25dfc83
Russell Dwarshuis <rjd@merit.edu>
2013-10-23 18:17:51 -0400

    start of telnet data collection.  Parser only works on ssh connections

---
+++ ZenPacks/community/CiscoPluggableOptics/libexec/telnet_parser.pl
@@ -0,0 +1,35 @@
+#!/usr/local/bin/perl
+
+use Net::Telnet::Cisco;
+
+my ($ip,$user,$pw,$sensor,$intf) = @ARGV;
+
+my $session = Net::Telnet::Cisco->new(Host => $ip);
+$session->login($user, $pw);
+
+# run command to find information
+my @output = $session->cmd("show int $intf tran detail");
+
+my $saw_sensor = 0;
+my $saw_dashes_after_sensor = 0;
+foreach my $line (@output) {
+  print "$line";
+  if ($line =~ /$sensor/i) {
+    $saw_sensor = 1;
+    next;
+  }
+  if ($saw_sensor && $line =~ /^-----/) {
+    $saw_dashes_after_sensor++;
+    # more dashes indicate start of next sensor type; didn't see $intf
+    if ($saw_dashes_after_sensor > 1) {
+      print "FAIL\n";
+      exit 3;
+    }
+    next;
+  }
+  if ($saw_dashes_after_sensor && $line =~ /^$intf\s+/i) {
+    my @val = split /\s+/,$line;
+    print "OK|rval=$val[1]\n";
+    exit 0;
+  }
+}
---
+++ ZenPacks/community/CiscoPluggableOptics/parsers/ShowIntTranDetail.py
@@ -0,0 +1,26 @@
+################################################################################
+#
+# This program is part of the CiscoPluggableOptics Zenpack for Zenoss.
+# Copyright (C) 2013 Russell Dwarshuis
+#
+# This program can be used under the GNU General Public License version 2
+# You can find full information here: http://www.zenoss.com/oss
+#
+################################################################################
+
+__doc__="""Log into a Cisco switch or router using telnet and issue commands
+to collect optical performance measurements.
+"""
+import re
+import pprint
+import logging
+from Products.ZenRRD.CommandParser import CommandParser
+
+
+logger = logging.getLogger('.'.join(['zen', __name__]))
+
+class ShowIntTranDetail(CommandParser):
+
+    def processResults(self, cmd, result):
+        log.debug('In processResults, got output: %s' %
+                  pprint.pformat(cmd.result.output))
