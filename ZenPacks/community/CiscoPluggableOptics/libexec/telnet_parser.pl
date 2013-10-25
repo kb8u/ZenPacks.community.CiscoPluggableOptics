74fbd6184c54d4e90b14ff0f6d0fa32f0bad706c
Russell Dwarshuis <rjd@merit.edu>
2013-10-25 14:11:43 -0400

    fixed problem with Celsius graph

--- ZenPacks/community/CiscoPluggableOptics/libexec/telnet_parser.pl
+++ ZenPacks/community/CiscoPluggableOptics/libexec/telnet_parser.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 
 use Net::Telnet::Cisco;
 
@@ -13,7 +13,8 @@ my @output = $session->cmd("show int $intf tran detail");
 my $saw_sensor = 0;
 my $saw_dashes_after_sensor = 0;
 foreach my $line (@output) {
-  print "$line";
+  # Net::Telnet::Cisco gets confused and puts newline at beginning sometimes
+  $line =~ s/^\s+//;
   if ($line =~ /$sensor/i) {
     $saw_sensor = 1;
     next;
