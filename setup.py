42cc926f1d225aaa1ced9234072f4b4b60d01e59
Russell Dwarshuis <rjd@merit.edu>
2014-12-15 11:28:03 -0500

    Fixed rrd template python class, removed old telnet code

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.CiscoPluggableOptics"
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 AUTHOR = "Russell Dwarshuis"
 LICENSE = "GPLv2+"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
